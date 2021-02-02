38eff9a695e6acc022b60cbbbfc8f79b1e63fcda
Muhammad Waleed Khan <fastnegative1004@gmail.com>
2021-02-02 14:10:55 +0500

    Form UI
    working on app.js and app.css file and created rough.txt file for unusual stuff

--- src/App.js
+++ src/App.js
@@ -4,25 +4,7 @@ import backGround from "./Components/Images/bg.svg";
 import Wave from "./Components/Images/wave.png";
 
 function App() {
-  const inputs = document.querySelectorAll(".input");
 
-  function addcl() {
-    let parent = this.parentNode.parentNode;
-    parent.classList.add("focus");
-  }
-
-  function remcl() {
-    let parent = this.parentNode.parentNode;
-    if (this.value === "") {
-      parent.classList.remove("focus");
-    }
-  }
-
-
-  inputs.forEach(input => {
-    input.addEventListener("focus", addcl);
-    input.addEventListener("blur", remcl);
-  });
   return (
     <div>
       <img className="wave" src={Wave} alt="Wave" />
@@ -31,7 +13,7 @@ function App() {
           <img src={backGround} alt="Background" />
         </div>
         <div className="login-content">
-          <form action="index.html">
+          <form action="./index.html">
             <img src={Avatar} alt="Avatar" />
             <h2 className="title">Welcome</h2>
             <div className="input-div one">
@@ -40,7 +22,7 @@ function App() {
               </div>
               <div className="div">
                 <h5>Username</h5>
-                <input type="text" className="input" />
+                <input type="text" className="inputs" />
               </div>
             </div>
             <div className="input-div pass">
@@ -49,10 +31,10 @@ function App() {
               </div>
               <div className="div">
                 <h5>Password</h5>
-                <input type="password" className="input" />
+                <input type="password" className="inputs" />
               </div>
             </div>
-            <a href="#">Forgot Password?</a>
+            {/* <a href="#">Forgot Password?</a> */}
             <input type="submit" className="btn" defaultValue="Login" />
           </form>
         </div>
